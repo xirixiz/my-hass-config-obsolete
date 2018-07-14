cf30cb682cff775f55be83eccf89b880ea84497e
Bram van Dartel <spam@rootrulez.com>
2018-07-14 07:39:01 +0200

    minor changes to lights and lovelace scaling

--- custom_components/sensor/test_mijnafvalwijzer.py
+++ custom_components/sensor/test_mijnafvalwijzer.py
@@ -8,6 +8,7 @@
 from datetime import datetime, timedelta
 import requests
 import logging
+import json
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -49,10 +50,11 @@ today = datetime.today().strftime("%Y-%m-%d")
 dateConvert = datetime.strptime(today, "%Y-%m-%d") + timedelta(days=1)
 tomorrow = datetime.strftime(dateConvert, "%Y-%m-%d")
 
+trash = {}
 trashType = {}
-tschedule = []
-trashTomorrow = {}
 trashToday = {}
+trashTomorrow = {}
+tschedule = []
 
 for name in trashTotal:
     for item in json_data or json_data_next:
@@ -63,7 +65,7 @@ for name in trashTotal:
         if name not in trashType:
             if item['date'] == today:
                 trashToday = {}
-                trashType[name] = "today"
+                trashType[name] = today
                 trashToday['name_type'] = "today"
                 trashToday['pickup_date'] = item['nameType']
                 tschedule.append(trashToday)
@@ -75,6 +77,13 @@ for name in trashTotal:
                 trashTomorrow['pickup_date'] = item['nameType']
                 tschedule.append(trashTomorrow)
 
+            if item['date'] <= today:
+                trash = {}
+                trashType[name] = item["nameType"]
+                trash['name_type'] = item['nameType']
+                trash['pickup_date'] = "None"
+                tschedule.append(trash)
+
             if item['date'] >= today:
                 trash = {}
                 trashType[name] = item["nameType"]
@@ -82,18 +91,43 @@ for name in trashTotal:
                 trash['pickup_date'] = dateConvert
                 tschedule.append(trash)
 
-if len(trashToday) == 0:
-    trashToday = {}
-    trashType[name] = "today"
-    trashToday['name_type'] = "today"
-    trashToday['pickup_date'] = "None"
-    tschedule.append(trashToday)
 
-if len(trashTomorrow) == 0:
-    trashTomorrow = {}
-    trashType[name] = "tomorrow"
-    trashTomorrow['name_type'] = "tomorrow"
-    trashTomorrow['pickup_date'] = "None"
-    tschedule.append(trashTomorrow)
+#if len(trashToday) == 0:
+#    trashToday = {}
+#    trashType[name] = "today"
+#    trashToday['name_type'] = "today"
+#    trashToday['pickup_date'] = "None"
+#    tschedule.append(trashToday)
+
+#if len(trashTomorrow) == 0:
+#    trashTomorrow = {}
+#    trashType[name] = "tomorrow"
+#    trashTomorrow['name_type'] = "tomorrow"
+#    trashTomorrow['pickup_date'] = "None"
+#    tschedule.append(trashTomorrow)
+
+#print(tschedule)
+
+json_str = json.dumps(tschedule)
+json_rsp = json.loads(json_str)
+
+
+#tschedule = [{'name_type': 'papier', 'pickup_date': '2018-07-18'}, {'name_type': 'gft', 'pickup_date': '2018-07-20'}, {'name_type': 'pmd', 'pickup_date': '2018-07-26'}, {'name_type': 'restafval', 'pickup_date': '2018-07-27'}]
+
+#for x in tschedule:
+#    print(x['name_type'])
+#    if x['name_type'] == "tomorrow":
+#        retval = True
+#        print(x)
+#        break
+
+
+
 
 print(tschedule)
+
+#list = ['350882 348521 350166\r\n']
+#id = 348522
+#if id not in [int(y) for x in list for y in x.split()]:
+#    list.append(id)
+#print (list)
